lourkeur/devos

user/xcape: take on lshift

users/profiles/xcape/default.nix
@@ -5,6 +5,9 @@
       "Alt_L" = "Escape";
       "Control_R" = "Tab";
       "Shift_R" = "Shift_R|Tab";
+
+      # Compensate the lack of a ≷ on ANSI boards
+      "Shift_L" = "backslash";
     };
   };
 }

lourkeur/config

users/profiles/xcape/default.nix
@@ -5,6 +5,9 @@
       "Alt_L" = "Escape";
       "Control_R" = "Tab";
       "Shift_R" = "Shift_R|Tab";
+
+      # Compensate the lack of a ≷ on ANSI boards
+      "Shift_L" = "less";
     };
   };
 }